9164b0d573036a134de279ac5e31af63b8cfb9db
nagaraj-gandge <51735832+nagaraj-gandge@users.noreply.github.com>
2020-10-06 17:40:52 +0530

    Update hpe3par_volume.py

--- library/hpe3par_volume.py
+++ library/hpe3par_volume.py
@@ -5,7 +5,7 @@
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 3 of the GNU General Public License as
 # published by the Free Software Foundation.  Alternatively, at your
-# choice, you may also redistribute it and/or modify it under the terms
+# choice, you may also redistribute it at and/or modify it under the terms
 # of the Apache License, version 2.0, available at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
